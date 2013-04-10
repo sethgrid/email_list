35accf671f95eb6a8cc7abb83526f6923d9cbe54
Seth Ammons <seth.ammons@sendgrid.com>
2013-04-09 20:28:55 -0700

    created the lists models and added lists to settings.py under APPS

--- lists/models.py
+++ lists/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Email(models.Model):
+    email_address = models.CharField(max_length=80)
+
+class Sender(models.Model):
+    name = models.CharField(max_length=4)
+
+class List(models.Model):
+    sender = models.ForeignKey(Sender)
+    recipient = models.ForeignKey(Email)
+    unsubscribed = models.IntegerField(default=0)
