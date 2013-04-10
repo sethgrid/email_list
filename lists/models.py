eff6688bdbe9356c1d9eed77b682ba20721c121a
Seth Ammons <seth.ammons@sendgrid.com>
2013-04-10 07:03:29 -0700

    added unicode methods to model for shell interaction

--- lists/models.py
+++ lists/models.py
@@ -5,10 +5,17 @@ from django.db import models
 class Email(models.Model):
     email_address = models.CharField(max_length=80)
 
+    def __unicode__(self):
+        return self.email_address
+
 class Sender(models.Model):
     name = models.CharField(max_length=4)
+
+    def __unicode__(self):
+        return self.name
 
 class List(models.Model):
     sender = models.ForeignKey(Sender)
     recipient = models.ForeignKey(Email)
     unsubscribed = models.IntegerField(default=0)
+
