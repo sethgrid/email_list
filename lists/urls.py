4616a5471754d91afecf37a90a20a8d0d6964c87
Seth Ammons <seth.ammons@sendgrid.com>
2013-04-11 18:42:07 -0700

    endpoints for /lists/{sender}/subscribes and unsubscribes is up

--- lists/urls.py
+++ lists/urls.py
@@ -4,5 +4,7 @@ from lists import views
 
 urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
-    url(r'^getlist/(?P<sender_name>\w+)/$', views.get_list, name='get_list')
+    url(r'^getlist/(?P<sender_name>\w+)/$', views.get_list, name='get_list'),
+    url(r'^getlist/(?P<sender_name>\w+)/subscribes/$', views.get_subscribed, name='get_subscribed'),
+    url(r'^getlist/(?P<sender_name>\w+)/unsubscribes/$', views.get_unsubscribed, name='get_unsubscribed')
 )
--- lists/views.py
+++ lists/views.py
@@ -11,19 +11,22 @@ def index(request):
 # get_subscribed(request, sender_name)
 # get_unsubscribed(request, sender_name)
 
-def get_list(request, sender_name=None):
+def get_list(request, sender_name=None, unsubscribed=-1):
     sender = _get_sender(sender_name)
     if not sender:
         return _send_error("No Sender Found", 404)
 
-    data = {"sender": sender.name}
+    emails = _get_emails_from_sender(sender.id, unsubscribed)
+    data = {"sender": sender.name,
+            "emails": emails
+           }
     return _send_response(data, 200)
 
 def get_subscribed(request, sender_name=None):
-    pass
+    return get_list(request, sender_name, 0)
 
 def get_unsubscribed(request, sender_name=None):
-    pass
+    return get_list(request, sender_name, 1)
 
 def _get_sender(sender_name):
     # get the first result back for the requested sender name
@@ -32,6 +35,19 @@ def _get_sender(sender_name):
     except IndexError:
         sender = None
     return sender
+
+def _get_emails_from_sender(sender_id, unsubscribedd=0):
+    emails = []
+    if unsubscribedd is -1:
+        list = List.objects.filter(sender=sender_id)
+    else:
+        list = List.objects.filter(sender=sender_id, unsubscribed=unsubscribedd)
+
+    for list_obj in list:
+        print list_obj.unsubscribed, 'is the unsubscribed for ', list_obj.recipient.email_address
+        emails.append(list_obj.recipient.email_address )
+
+    return emails
 
 def _send_error(message, status_code):
     data = {"error": message}
