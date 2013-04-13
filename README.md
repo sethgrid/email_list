798c322f710a3690e016263d784fc61ad57c9d71
Seth Ammons <seth.ammons@sendgrid.com>
2013-04-12 23:05:26 -0700

    added $ python manage.py populate [drop] and expanded readme

--- README.md
+++ README.md
@@ -4,3 +4,22 @@ Planning on putting some simple REST API endpoints up for educational purposes. 
 ## Guides followed
 https://devcenter.heroku.com/articles/django#prerequisites
 https://docs.djangoproject.com/en/1.5/intro/tutorial01/
+
+## Setup
+Currently hard coded to use postgres with user 'seth', no password, and database name 'seth' for local development. If deployed to Heroku, set the DATABASE_URL to your postgres instance.
+
+### Endpoints
+Current endpoints:
+/lists/{{sender}} - gets all emails in sender's list
+/lists/{{sender}}/subscribes - gets all subscribed emails
+/lists/{{sender}}/unsubscribes - gets all unsubscribed emails
+
+### Seed data
+$ python manage.py help populate
+Usage: manage.py populate [options] drop
+
+$ python manage.py populate
+Populates database with seed data of Senders A, B, C, D, and E. Populates 200 email addresses spread between them with some addresses shared and some unsubscribed.
+
+$ python manage.py populate drop
+drops data from the database prior to populating the database as normal
---
+++ lists/management/commands/populate.py
@@ -0,0 +1,70 @@
+from django.core.management.base import BaseCommand, CommandError
+from lists.models import Sender, Email, List
+
+class Command(BaseCommand):
+    args = 'drop'
+    help = ''''
+    $ python manage.py populate 
+    Populates database with seed data of Senders A, B, C, D, and E. Populates 200 email addresses spread between them with some addresses shared and some unsubscribed.
+    
+    $ python manage.py populate drop
+    drops data from the database prior to populating the database as normal
+    '''
+
+    def handle(self, *args, **options):
+        emails_to_create = 200
+        percent_chance_to_add_to_list = 75
+        percent_chance_to_unsub = 25
+
+        # if drop is set, remove all relevant data
+        if args and args[0] == 'drop':
+            Sender.objects.all().delete()
+            Email.objects.all().delete()
+            List.objects.all().delete()
+
+            self.stdout.write("Data dropped")
+
+        # if sender does not exist, create
+        names = ['A', 'B', 'C', 'D', 'E']
+        for name in names:
+            sender = Sender.objects.filter(name=name)
+            if not sender:
+                sender = Sender()
+                sender.name = name
+                sender.save()
+
+        senders = Sender.objects.all()
+
+        self.stdout.write("Senders created")
+
+        # create the emails and assign to senders in List
+        for i in range(emails_to_create):
+            import random
+
+            # slight chance for data collision, acceptable here
+            address = ''.join(random.choice('0123456789abcdefghijklmnopqrstuvwxyz') for i in range (8))
+            email = Email()
+            email.email_address = address + '@example.com'
+            email.save()
+
+            # go through each sender...
+            for i in range(len(senders)):
+                # chance to attach this email with this sender
+                self.stdout.write("attempt sender %s (%s)" % (senders[i].name, i))
+                if int(random.randrange(100)) > percent_chance_to_add_to_list:
+                    self.stdout.write("Yes!")
+                    sender_list = List()
+                    sender_list.sender = senders[i]
+                    sender_list.recipient = email
+
+                    # chance to have this email unsubscribed from this sender
+                    if int(random.randrange(100)) < percent_chance_to_unsub:
+                        sender_list.unsubscribed = 1
+                        self.stdout.write("unsubed")
+
+                    self.stdout.write("save")
+                    sender_list.save()
+
+        self.stdout.write("Email addresses created and assigned to senders")
+
+# make sure that the standard use cases are covered
